20dcdefebb396e72c50aeba653f28bd264d9bd3f
James <james.hallam93@gmail.com>
2021-03-23 13:35:10 +0000

    initial commit, stable

---
+++ src/index.js
@@ -0,0 +1,135 @@
+
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+const hasWon = (board, symbol) => {
+    let winner = false
+
+    winningCombos.map(combo => {
+        if (board[combo[0]] === symbol && board[combo[1]] === symbol && board[combo[2]] === symbol) {
+            winner = true
+        }
+    })
+    return winner
+}
+
+const hasDrawn = (board) => {
+    let nonEmptySquares = board.filter(x => x)
+
+    if (nonEmptySquares.length === board.length) {
+        return true
+    }
+    return false
+
+}
+
+export const evaluateBoard = (board) => {
+
+    const cpuSymbol = "X"
+    const playersSymbol = "O"
+
+    if (hasWon(board, playersSymbol)) {
+        return -10
+    }
+
+    if (hasWon(board, cpuSymbol)) {
+        return 10
+    }
+    return 0
+
+}
+const returnEmptyIndexes = (board) => {
+
+    let emptyIndexes = []
+
+    board.forEach((square, index) => {
+        if (square === "") {
+            emptyIndexes.push(index)
+        }
+    })
+
+    return emptyIndexes
+}
+
+export const bestMove = (board, currentSymbol) => {
+
+    let newBoard = board.slice()
+    const emptyIndexes = returnEmptyIndexes(board)
+
+
+    let bestValue = -1000
+
+    const bestMove = emptyIndexes.reduce((currentBest, emptyIndex) => {
+        newBoard[emptyIndex] = currentSymbol
+        const newValue = minimax(newBoard, 0, true)
+
+        newBoard[emptyIndex] = ""
+
+        if (newValue > bestValue) {
+            bestValue = newValue
+            return emptyIndex
+        }
+        return currentBest
+    }, -1)
+
+    return bestMove;
+}
+
+export const minimax = (board, depth, playersTurn) => {
+
+    let score = evaluateBoard(board)
+
+    if (score === -10) {
+        return score
+    }
+    if (score === 10) {
+        return score - depth
+    }
+    if (hasDrawn(board)) {
+        return 0;
+    }
+
+    if (!playersTurn) {
+        const bestMaximValue = board.reduce((currentBest, square, index) => {
+            let newBoard = board.slice()
+
+            if (square === "") {
+                newBoard[index] = "X"
+                const newDepth = depth + 1
+                const newValue = minimax(newBoard, newDepth, true)
+
+                if (newValue > currentBest) {
+                    return newValue
+                }
+            }
+            return currentBest
+        }, -1000)
+        return bestMaximValue
+    }
+
+    const bestMinimValue = board.reduce((currentBest, square, index) => {
+        let newBoard = board.slice()
+
+        if (square === "") {
+            newBoard[index] = "O"
+            const newDepth = depth + 1
+            const newValue = minimax(newBoard, newDepth, false)
+
+            if (newValue < currentBest) {
+                return newValue
+            }
+        }
+        return currentBest
+    }, 1000)
+    return bestMinimValue
+}
+
+module.exports = { bestMove, evaluateBoard, minimax }
---
+++ src/tests/index.test.js
@@ -0,0 +1,82 @@
+const bestMove = require("../index").bestMove
+const evaluateBoard = require("../index").evaluateBoard
+const minimax = require("../index").minimax
+
+//use test board for novel test cases - ai logic and decisions will be logged allowing for greater transparency
+let testBoard = [
+    "O", "", "X",
+    "", "", "",
+    "", "", "X"
+]
+
+let cpuWinBoard = [
+    "O", "", "X",
+    "", "", "",
+    "", "", "X"
+]
+
+let playerWinBoard = [
+    "O", "O", "",
+    "", "", "",
+    "", "", ""
+]
+//
+let earlyGameBoard = [
+    "O", "", "",
+    "", "", "",
+    "", "", ""
+]
+let emptyBoard = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+]
+
+const cpuMove = bestMove(testBoard, "X")
+
+//pure logging test, no expectations.
+test("the best move for the testBoard is", () => {
+
+    console.log(`on the test board, bestMove function has given square ${cpuMove + 1}`)
+    console.log("which gives the following board:")
+    testBoard[cpuMove] = "X"
+
+    const row1 = testBoard.slice(0, 3)
+    const row2 = testBoard.slice(3, 6)
+    const row3 = testBoard.slice(6, 9)
+    console.log(`\n ${row1} \n ${row2} \n ${row3}`)
+    
+    const score = minimax(testBoard, 0, true)
+    console.log(`With a score of ${score}`);
+    testBoard[cpuMove] = ""
+})
+
+describe("The best move function, relying on minimax algorithm chooses correct square in the following scenarios:", () => {
+
+    test("a board in which the cpu can win in one turn", () => {
+        expect(bestMove(cpuWinBoard, "X")).toEqual(5)
+    })
+
+    test("a board in which the player can win in one turn", () => {
+        expect(bestMove(playerWinBoard, "X")).toEqual(2)
+    })
+
+    test("an early game board in which the correct move is centre square", () => {
+        expect(bestMove(earlyGameBoard, "X")).toEqual(4)
+    })
+
+    test("an empty board (no `best move', will default to zero)", () => {
+        expect(bestMove(emptyBoard, "X")).toEqual(0)
+    })
+
+    test("given an empty board and THREE turns, will create a winning terminal state", () => {
+        const move1 = bestMove(emptyBoard, "X")
+        emptyBoard[move1] = "X"
+        const move2 = bestMove(emptyBoard, "X")
+        emptyBoard[move2] = "X"
+        const move3 = bestMove(emptyBoard, "X")
+        emptyBoard[move3] = "X"
+        expect(evaluateBoard(emptyBoard)).toEqual(10)
+    })
+})
+
